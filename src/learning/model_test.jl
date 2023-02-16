816ad44fe2a1f2c0b316e73c26fbece038deb931
Pasha Khosravi <khosravipasha@gmail.com>
2023-02-15 20:32:56 -0800

    model test

--- src/learning/model_test.jl
+++ src/learning/model_test.jl
@@ -2,18 +2,28 @@ using Flux
 using CUDA
 using BSON: @load
 using ProbabilisticCircuits
-include("models.jl")
+using Revise
+includet("../models.jl")
 
-pc = Base.read("mnist35.jpc", ProbCircuit);
-CUDA.@time bpc = CuBitsProbCircuit(pc);
 
-@load "src/model/flux_NN_MNIST.bson" model
-train_data, test_data=data_pre()
-train_data = train_data[1][1]
-train_data = collect(train_data)
-println(size(train_data))
+function train_model(df)
+    train_data, test_data=data_pre(df)
+    CUDA.@time begin
+        model = Chain(
+            Dense(784=>128, relu),
+            Dense(128=>10, relu),
+            Dense(10=>1, sigmoid)
+            )
+        loss(x, y) = Flux.binarycrossentropy(model(x), y)
+        opt = Flux.Optimise.ADAM()
+        for epochs in 1:5
+            Flux.train!(loss, Flux.params(model), train_data, opt)
+        end
+    end
+    return model
+end
 
-function run(batch_size=5000)
+function run(model, batch_size=100)
     println("====================")
     println("Train Data Size", size(train_data))
     train_batch = train_data[:, 1:batch_size]
@@ -23,12 +33,16 @@ function run(batch_size=5000)
     model_gpu = gpu(model)
     CUDA.@time model_gpu(train_batch_gpu)
     CUDA.@time model_gpu(train_batch_gpu)
+
+    nothing
 end
 
-function run_3d(sample_size, beam_size)
+function run_3d(model, sample_size, beam_size)
     data_gpu = cu(permutedims(train_data[:, 1:beam_size], [2, 1])) # just want the correct shape for now, (beam_size, features)
     S_gpu = ProbabilisticCircuits.sample(bpc, sample_size, data_gpu)    # -> (sample_size, data_size, num_features)
     
+    @show typeof(S_gpu)
+
     S = Array(S_gpu)
     S2 = permutedims(S, [3, 1, 2]) # (num_features, sample_size*, data_size*); only matters that num_features is first for using Flux
     @show size(S)
@@ -41,8 +55,10 @@ function run_3d(sample_size, beam_size)
 
     # 1. premute dims to match the need for the NN, num_features should be first 
     # 2. convert from CuArray{Missing, Int64} -> CuArray{Int64}
-    S2_gpu = convert(CuArray{Int64}, permutedims(S_gpu, [3, 1, 2]))::CuArray{Int64}
-    @show typeof(S2_gpu)  
+    CUDA.@time S2_gpu = permutedims(S_gpu, [3, 1, 2])::CuArray{Union{Int64, Missing}}
+    @show typeof(S2_gpu)
+    CUDA.@time S2_gpu = convert(CuArray{Int64}, S2_gpu)
+    @show typeof(S2_gpu)
     @show size(S2_gpu)
     model_gpu = gpu(model)
 
@@ -53,6 +69,20 @@ function run_3d(sample_size, beam_size)
     return nothing
 end
 
-# run()
-# run_3d(100, 28*28*3)
+
+pc = Base.read("circuits/mnist35.jpc", ProbCircuit);
+CUDA.@time bpc = CuBitsProbCircuit(pc);
+
+
+df = DataFrame(CSV.File("data/mnist_3_5_test.csv"))
+train_data, test_data=data_pre(df)
+train_data = train_data[1][1]
+train_data = collect(train_data)
+@show size(train_data)
+
+# @load "src/model/flux_NN_MNIST.bson" model
+model = train_model(df);
+
+# run(model)
+# run_3d(model, 100, 28*28*3)
 
--- src/models.jl
+++ src/models.jl
@@ -45,40 +45,6 @@ function binvec(x::AbstractVector, n::Int,
     return df.binids
 end
 
-#= function train_LR()
-    df = DataFrame(CSV.File("data/data.csv"))
-    select!(df, Not([:id]))
-    for column in names(df)
-        if column!="diagnosis"
-            df[!,column]=binvec(df[!,column],5)
-        end
-    end
-    train, test = splitdf(df, 0.6);
-    train=Matrix(train)
-    test=Matrix(test)
-    model = LogisticRegression(fit_intercept=true,max_iter=500)
-    y_train=train[:,1]
-    X_train=train[:,2:end]
-    y_test=test[:,1]
-    X_test=test[:,2:end]
-    ScikitLearn.fit!(model, X_train, y_train)   
-    prediction = ScikitLearn.predict(model, X_test)
-    prediction_class = [if x < 0.5 0 else 1 end for x in prediction];
-  
-    prediction_df = DataFrame(y_actual = y_test,y_predicted = prediction_class, prob_predicted = ScikitLearn.predict_proba(model, X_test)[:,2]);
-    prediction_df.correctly_classified = prediction_df.y_actual .== prediction_df.y_predicted
-    accuracy = mean(prediction_df.correctly_classified)
-    #temp=sum(ScikitLearn.predict_proba(model, X_test)[:,2])
-    #println(temp)
-    println(accuracy)
-    #println(first(prediction_df,2))
-    #instance=Array(test[test[:,1] .== 0,:])
-    #i=instance[2,:]
-    #popfirst!(i)
-    #println(i)
-    return model
-end =#
-
 function data_pre(df)
     train, test = splitdf(df, 0.6);
     train=Matrix(train)
@@ -144,7 +110,9 @@ function train_LR_flux()
         )
     loss(x, y) = Flux.binarycrossentropy(model(x), y)
     opt = Flux.Optimise.ADAM()
-    Flux.@epochs 250 Flux.train!(loss, Flux.params(model), train_data, opt)
+    for i = 1:250
+        Flux.train!(loss, Flux.params(model), train_data, opt)
+    end
     
     return model, test_data
 end
